6a2e3d39d2b4877601446b0697cf7c6e24eb4478
DESKTOP-RQCCB4A\Flurrih <ghost744@gmail.com>
2019-10-27 22:52:43 +0100

    Add simplereddit scrapper

--- RedditScrapper.py
+++ RedditScrapper.py
@@ -1,4 +1,33 @@
 #! python3
 import praw
 import pandas as pd
-import datetime as dt
+
+reddit = praw.Reddit(client_id='', \
+                     client_secret='', \
+                     user_agent='', \
+                     username='', \
+                     password='')
+
+
+subreddit = reddit.subreddit('Nootropics')
+
+top_subreddit = subreddit.top()
+
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], "url":[], \
+                "comms_num": [], \
+                "created": [], \
+                "body":[]}
+
+for submission in top_subreddit:
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["comms_num"].append(submission.num_comments)
+    topics_dict["created"].append(submission.created)
+    topics_dict["body"].append(submission.selftext)
+
+topics_data = pd.DataFrame(topics_dict)
+topics_data.to_html('htm.html')
